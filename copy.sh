fcc2d97cd379941f79ae04406d14973f800deddc
TrungTN <trungtn83@gmail.com>
2024-08-06 23:15:51 +0700

    Rename copy folder

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./gradlew build 
-scp "build/libs/dpvn-eureka-server-0.0.1-SNAPSHOT.jar" root@103.1.239.43:/Users/reportnew/eureka
+scp "build/libs/dpvn-eureka-server-0.0.1-SNAPSHOT.jar" root@103.1.239.43:/apps/eureka
 
 echo "Build and Copy progress completed."
