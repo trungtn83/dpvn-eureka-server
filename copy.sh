d22d680f3d28f76b9b7d509fc5884a4c0dbf81bb
TrungTN <trungtn83@gmail.com>
2025-03-16 00:50:06 +0700

    change ip

--- copy.sh
+++ copy.sh
@@ -6,7 +6,7 @@
 # Check if the build succeeded
 if [ $? -eq 0 ]; then
     # If the build succeeded, copy the jar file
-    scp "build/libs/dpvn-eureka-server-0.0.1-SNAPSHOT.jar" root@160.30.112.181:/apps/eureka
+    scp "build/libs/dpvn-eureka-server-0.0.1-SNAPSHOT.jar" root@160.30.112.68:/apps/eureka
     echo "Build and Copy progress completed."
 else
     # If the build failed, print an error message and exit
